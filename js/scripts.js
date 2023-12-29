00a7e2854b5165454bbb23e433a40335c248bb21
Rio Rizki Aryanto <rio.aryanto.thrive@gmail.com>
2023-12-29 09:57:41 +0700

    Initial staging

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener('scroll', function() {
+    const container = document.querySelector('.container');
+    container.style.height = 'auto';
+    container.style.height = container.scrollHeight + 'px';
+  });
